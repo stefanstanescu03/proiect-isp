86ff59417a8d86c17fef06db24581f2418571a6d
BeriLeonard <berianuleonard87@gmail.com>
2025-05-13 17:16:19 +0300

    Am adaugat inregistrareNota pentru Profesori

--- src/proiect_isp/Profesor.java
+++ src/proiect_isp/Profesor.java
@@ -2,12 +2,15 @@ package proiect_isp;
 
 import java.util.List;
 
+
 public class Profesor extends Cont{
 	private List<Curs> cursuriPredate;
+	private List<Material> materialeIncarcate;
 
-	public Profesor(String parola, String nume, String prenume, String email, String telefon,) {
+	public Profesor(String parola, String nume, String prenume, String email, String telefon) {
 		super(parola, nume, prenume, email, telefon);
-		cursuriPredate = new List<Curs>();
+		this.cursuriPredate = new List<Curs>;
+		this.materialeIncarcate = new List<Material>;
 	}
 
 	public List<Curs> getCurrsuriPredate() {
@@ -20,6 +23,31 @@ public class Profesor extends Cont{
 
 	public void setCurrsuriPredate(List<Curs> cursuriPredate) {
 		this.cursuriPredate = cursuriPredate;
+	}
+	
+	public void adaugareMaterial(Material material) {
+		this.materialeIncarcate.add(material);
+	}
+	
+	public void inregistrareNota(String curs, String nume, String prenume, int nota) {
+		for (Curs curs : cursuriPredate) {
+			if(curs.getDenumire()==curs) {
+				for (Student student : curs.getStudenti())
+					if(student.getNume()==nume && student.getPrenume()==prenume) {
+						Date data = new Date();
+						Nota nota = new Nota(curs, data, nota);
+						student.adaugareNota(nota);
+					}
+					else {
+						System.out.println("Acest student nu este inrolat la acest curs!");
+					}
+			}
+			else {
+				System.out.println("Acest profesor nu preda acest curs!");
+				
+			}
+				
+		}
 	}
 	
 }
