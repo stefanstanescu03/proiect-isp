1b604a53a04e40c263550c26b8a7f9cc5f46b843
Stefan Stanescu <st.stefan24@gmail.com>
2025-05-13 18:44:25 +0300

    am facut cautaMateriale()

--- src/proiect_isp/Student.java
+++ src/proiect_isp/Student.java
@@ -2,6 +2,7 @@ package proiect_isp;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.time.LocalDate;
 
 public class Student extends Cont{
 	private int anUniversitar;
@@ -51,6 +52,67 @@ public class Student extends Cont{
 	public void afisare() {
 		super.afisare();
 		System.out.println("An: " + anUniversitar );
+	}
+	
+	public List<Material> cautaMateriale(Curs curs) {
+		if (cursuri.indexOf(curs) == -1) {
+			return null;
+		}
+		
+		List<Material> materialeRecomandate = new ArrayList<Material>();
+		
+		if (anUniversitar == curs.getAn()) {
+			 LocalDate today = LocalDate.now();
+			 LocalDate jumatateSem1 = LocalDate.of(today.getYear() - 1, 12, 20);
+			 LocalDate inceputSem2 = LocalDate.of(today.getYear(), 2, 24);
+			 LocalDate jumatateSem2 = LocalDate.of(today.getYear(), 4, 7);
+		     
+		     if (today.isBefore(jumatateSem1) || (today.isAfter(inceputSem2) && today.isBefore(jumatateSem2))) {
+		    	 for (Material material : curs.getMateriale()) {
+					if (material.getDificultate() < 5) {
+						materialeRecomandate.add(material);
+					}
+				}
+		     } else {
+		    	 for (Material material : curs.getMateriale()) {
+					if (material.getDificultate() >= 5 && material.getDificultate() < 8) {
+						materialeRecomandate.add(material);
+					}
+				}
+		     }
+		     
+		} else {
+			int notaObtinuta = -1;
+			for (Nota nota : note) {
+				if (nota.getCurs() == curs) {
+					notaObtinuta = nota.getValoare();
+					break;
+				}
+			}
+			if (notaObtinuta < 5) {
+				for (Material material : curs.getMateriale()) {
+					if (material.getDificultate() < 5) {
+						materialeRecomandate.add(material);
+					}
+				}
+			} else {
+				if (notaObtinuta >= 5 && notaObtinuta < 8) {
+					for (Material material : curs.getMateriale()) {
+						if (material.getDificultate() >= 5 && material.getDificultate() < 8) {
+							materialeRecomandate.add(material);
+						}
+					}
+				} else {
+					for (Material material : curs.getMateriale()) {
+						if (material.getDificultate() >= 8) {
+							materialeRecomandate.add(material);
+						}
+					}
+				}
+			}
+		}
+		
+		return materialeRecomandate;
 	}
 	
 }
